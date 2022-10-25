dae6d1993bc44c6df10652aea08962cabf9f6ec6
Adam8J <adam.jurasz@wp.pl>
2022-10-25 20:08:48 +0200

    kotki

--- src/For.java
+++ src/For.java
@@ -1,7 +1,11 @@
 public class For {
     public static void main(String[] args) {
-        for (int i = 0; i < 10; i=i+2){
+        for (int i = 0; i < 2; i=i+2){
             System.out.println("cześć Adam");
+        }
+        String [] catTable = new String[] {"Mruczek", "Buczek", "Leon", "Julian"};
+        for (int i = 0; i <catTable.length; i++){
+            System.out.println(catTable[i]);
         }
     }
 }
