9cc142326ccd6c05a1a5975a1ad37019ba5f7577
Adam8J <adam.jurasz@wp.pl>
2022-10-25 21:03:01 +0200

    boolean

---
+++ src/Domowe.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+
+public class Domowe {
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        System.out.println("podaj imię");
+        String imie=sc.next();
+        System.out.println("zonaty");
+        boolean stanCywilny=sc.nextBoolean();
+    }
+}
