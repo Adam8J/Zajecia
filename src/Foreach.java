227417165659d3084c715135ed72080e56549533
Adam8J <adam.jurasz@wp.pl>
2022-10-25 20:17:07 +0200

    kotki czter

---
+++ src/Foreach.java
@@ -0,0 +1,8 @@
+public class Foreach {
+    public static void main(String[] args) {
+        String [] catTable = new String[] {"Mruczek", "Buczek", "Leon", "Julian"};
+        for (String cat: catTable){
+            System.out.println(cat);
+        }
+    }
+}
